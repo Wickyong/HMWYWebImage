24cdda23c68607a98d0d2184cab8e78e1a936c90
WickYong <wy46817@gmail.com>
2017-06-23 18:52:16 +0800

    项目准备

---
+++ 仿SDWebImage/仿SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿SDWebImage
+//
+//  Created by WangYong on 2017/6/23.
+//  Copyright © 2017年 WangYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
